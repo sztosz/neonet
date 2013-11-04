dd938add389d85fa457457a37d6dfd500fb13b9c
Bartosz Nowak <sztosz@gmail.com>
2013-11-04 21:30:50 +0100

    New DamageReportForm for scanner entry

--- S/forms.py
+++ S/forms.py
@@ -17,3 +17,11 @@ class EanForm(qa_forms.EanForm):
 class DamageReportForm(forms.ModelForm):
     class Meta:
         model = models.DamageReport
+        fields = ('serial', 'brand', 'comment', 'net_value')
+        widgets = {
+            'detection_time': forms.CharField(max_length=30),
+            'category':       forms.CharField(max_length=1),
+            'further_action': forms.CharField(max_length=30),
+            'damage_kind':    forms.CharField(max_length=30),
+        }
+
