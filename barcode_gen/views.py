27640f4181559ac284c0576855eaa26a367faa1b
Bartosz Nowak <sztosz@gmail.com>
2014-02-09 13:13:15 +0100

    If ean is not valid proper svg is shown

--- barcode_gen/views.py
+++ barcode_gen/views.py
@@ -9,19 +9,17 @@
 
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from qa.tools import parsers
 
 
 def sgv_ean_barcode(request, ean=None):
-    response = HttpResponse(content_type='image/svg+xml')
-    # response['Content-Disposition'] = 'attachment; filename="quick_list.csv.txt"' ????
 
     if parsers.validate_ean13(ean):
-        import svgwrite
-        svg = svgwrite.Drawing(filename='ean_invalid.svg')
-        raise ValueError('EAN IS INVALID') # TODO create proper svg picture instead of raising error
+        return redirect('/static/img/ean_error.svg')
     else:
         import barcode
+        response = HttpResponse(content_type='image/svg+xml')
         options = {'module_height': 5, 'write_text': False}
         svg = barcode.get_barcode_class('ean13')(ean)
         response.write(svg.render(options))
